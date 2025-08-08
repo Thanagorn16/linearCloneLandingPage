8416d86991006fa5d50e908ca485a4e624bf1258
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2025-08-08 14:58:23 +0700

    fixed: header can be see through from dropdown menu / dropdown menu is closed instantly once mouse leaving the button area

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,43 @@
 import { useState } from "react";
 import { clsx } from 'clsx';
 
-const textBtn = `px-3 py-2 text-zinc-400 hover:bg-white/10 
-					text-sm font-light rounded-lg transition-colors 
-					cursor-pointer`
-
+const textBtn = (hover = 'hover:bg-white/10') => clsx(
+	`px-3 py-2 text-zinc-400 ${hover}
+	text-sm font-light rounded-lg transition-colors 
+	cursor-pointer`
+)
 
 function NavItem({label, dropdownItems}) {
 	const [open, setOpen] = useState(false);
 
 	return (
 		<div 
-			className="relative"
+			className="relative group"
 			onMouseEnter={() => setOpen(true)}
 			onMouseLeave={() => setOpen(false)}
 		>
-			<button className={textBtn}>{label}</button>
+			<button className={textBtn('group-hover:bg-white/10')}>{label}</button>
 
 			{open && (
-				<div className="absolute w-2xl h-20 py-39 mt-2 -translate-x-15 bg-[rgb(9,10,11)] border border-zinc-800 rounded-xl">
-						<div className="absolute top-2 left-3 right-3 bottom-17 flex box-border border-1 p-4 rounded-lg bg-white/10">
+				<div className="absolute top-full left-0 w-full h-4 bg-transparent"></div> 
+			)}
 
+			{open && (
+				<div className="absolute z-1 w-2xl h-20 py-39 mt-2 -translate-x-20 bg-[rgb(9,10,11)] border border-zinc-800 rounded-xl">
+					<div className="flex absolute top-2 left-2 right-2 bottom-17 box-border border border-zinc-800 p-4 rounded-lg bg-[#161616]">
+						<div className="absolute box-border size-50 px-2">
+							<span className="text-xs text-[#62666a]">Core Features</span>
+							<div className="flex flex-col">
+								<a className="">
+									<h2 className="text-sm mt-4">Plan</h2>
+									<p className="text-xs">Set the product direction with projects and initiatives</p>
+								</a>
+							</div>
 						</div>
+					</div>
 				</div>
 			)}
+
 		</div>
 	)
 } 
@@ -32,7 +46,7 @@ export default function Navbar() {
 	
 	return (
 		<nav className="mx-auto w-full px-2 sm:px6 lg:px-8 border border-b-zinc-800">
-			<div className="max-w-5xl px-4 mx-auto relative flex h-16 items-center justify-between group">
+			<div className="max-w-5xl px-4 mx-auto relative flex h-16 items-center justify-between ">
 				<div className="flex gap-2 items-center">
 					<img
 						className="w-6 h-6 fill-zinc-100"
@@ -42,18 +56,18 @@ export default function Navbar() {
 					<a className="text-lg font-medium text-zinc-100">Linear</a>
 				</div>
 
-				<div className="flex items-center gap-2 text-zinc-400 text-base font-sm">
+				<div className="flex top-[calc(100%+10px)] items-center gap-2 text-zinc-400 text-base font-sm">
 					<NavItem label={"Product"} dropdownItems={["Plan", "Building"]}/>
-					<a className={textBtn}>Resources</a>
-					<a className={textBtn}>Pricing</a>
-					<a className={textBtn}>Customers</a>
-					<a className={textBtn}>Now</a>
-					<a className={textBtn}>Contact</a>
+					<a className={textBtn()}>Resources</a>
+					<a className={textBtn()}>Pricing</a>
+					<a className={textBtn()}>Customers</a>
+					<a className={textBtn()}>Now</a>
+					<a className={textBtn()}>Contact</a>
 				</div>
 
 
 				<div className="flex items-center gap-4">
-					<button className={`${textBtn}`}>Log in</button>
+					<button className={`${textBtn()}`}>Log in</button>
 					<button className="px-3 py-2 bg-zinc-300 text-zinc-900 hover:bg-white text-sm rounded-lg transition-colors cursor-pointer">Sign up</button>
 				</div>
 			</div>
