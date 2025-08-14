cb04885bd4854025c02bd119cd8240469def67a0
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2025-08-14 13:21:21 +0700

    added: transition to customer display

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -27,10 +27,15 @@ export default function Customer() {
     const pairs = GetPair(customers);
 
     const [index, setIndex] = useState(0);
+    const [visible, setVisible] = useState(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex(prev => (prev + 1) % 2);
+            setVisible(false);
+            setTimeout(() => {
+                setIndex(prev => (prev + 1) % 2);
+                setVisible(true);
+            }, 300)
         }, 3000);
 
         return () => clearInterval(interval);
@@ -44,8 +49,10 @@ export default function Customer() {
             </p>
             <div className="grid grid-cols-3 gap-8 h-full py-15 justify-items-center">
                 {pairs.map((pair) => (
-                    <div className="w-full h-30 py-9 text-3xl text-center bg-amber-400">
-                        <span className="inline-block align-middle font-bold">{pair[index]}</span>
+                    <div className={`w-full h-30 py-9 text-3xl text-center bg-[#090a0b] transition-all duration-300 ease-in-out
+                        ${visible ? "opacity-100 scale-100" : "opacity-0 scale-90"}`}
+                    >
+                        <span className="inline-block align-middle font-bold text-[#f7f8f8]">{pair[index]}</span>
                     </div>
                 ))}
             </div>
