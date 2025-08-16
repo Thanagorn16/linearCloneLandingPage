755c593fb42f9d9c0e83ccacaa993d590823f5ac
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2025-08-16 17:19:44 +0700

    added: cards for purpose

--- src/components/Purpose.jsx
+++ src/components/Purpose.jsx
@@ -1,27 +1,66 @@
+function Card({image, alt, title}) {
+	return (
+		<div className="basis-64 group">
+			<a className="absolute w-90 h-90 group bg-[hsl(210,5%,8%)] rounded-xl transition-colors duration-200 group-hover:bg-[hsl(210,5%,10%)]">
+				{image && <img src={image} alt={alt}
+					className="w-full h-70 px-15 pt-10 pb-0 object-cover"
+				/>}
+			<div className="absolute flex left-6 w-80 justify-between items-center">
+				<span className="w-50 text-xl text-[#f7f8f8]">
+					{title}
+				</span>
+				<PlusSign/>
+			</div>
+			</a>
+		</div>
+	);
+}
+
 export default function Purpose() {
-    return (
+	return (
 		<section className="max-w-5xl mx-auto px-4 mt-25 ">
-            <div className="flex flex-row gap-x-20 ">
-                <div className="basis-1/2">
-                    <h1 className="text-6xl font-medium text-[#f7f8f8]">
-                        Made for modern product teams
-                    </h1>
-                </div>
+			<div className="flex flex-row gap-x-25">
+				<div className="basis-1/2">
+					<h1 className="text-6xl font-medium text-[#f7f8f8]">
+						Made for modern product teams
+					</h1>
+				</div>
 
-                <div className="basis-1/2 self-end">
-                    <p className="text-base text-[#8a8f98]">
-                        Linear is shaped by the practices and principles<br/>
-                        that distinguish world-class product teams from<br/>
-                        the rest: relentless focus, fast execution, and a<br/>
-                        commitment to the quality of craft.
-                        <a className="text-[#f7f8f8] cursor-pointer"> Make the switch</a>
-                    </p>
-                </div>
-            </div>
+				<div className="basis-1/2 self-end">
+					<p className="text-base text-[#8a8f98]">
+						Linear is shaped by the practices and principles<br/>
+						that distinguish world-class product teams from<br/>
+						the rest: relentless focus, fast execution, and a<br/>
+						commitment to the quality of craft.
+						<a className="text-[#f7f8f8] cursor-pointer"> Make the switch</a>
+					</p>
+				</div>
+			</div>
 
-            <div>
+			{/* cards */}
+			<div className="flex flex-row mt-15 gap-x-30">
+				<Card image="../../development.png" alt="development" title='Purpose-built for product development'/>
+				<Card image="../../speed.png" alt="speed" title='Designed to move fast'/>
+				<Card image="../../craft.png" alt="craft" title='Crafted to perfection'/>
+			</div>
 
-            </div>
-        </section>
-    );
+		</section>
+	);
+
+}
+
+function PlusSign() {
+	return (
+		<div className={`flex items-center justify-center w-10 h-10 rounded-full border-2 border-[hsl(240,5%,16%)] 
+			transition-border duration-300  group-hover:bg-[hsl(240,4%,16%)]`}>
+			<svg 
+				class="w-6 h-6 text-[hsl(219,6%,57%)] transition-colors duration-300 group-hover:text-[#f7f8f8]" 
+				aria-hidden="true" 
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 30 30"
+			>
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 13h14m-7 7V7"/>
+			</svg>
+		</div>
+	);
 }
