3f47e9bfe75ddc07312b5aa540d10c334275a48d
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2025-09-11 19:40:51 +0700

    added: nan window

---
+++ src/components/Imgs.jsx
@@ -0,0 +1,29 @@
+export function GetNanImg() {
+	return (
+		<img 
+			className="w-4 h-4 rounded-full"
+			src="https://webassets.linear.app/images/ornj730p/production/e522be49c136f6b81a1c5cb3aa7b2075f78404c0-1305x1444.jpg?w=72&amp;q=95&amp;auto=format&amp;dpr=2" 
+			alt="Avatar of Nan">
+		</img>
+	)
+}
+
+export function GetYannImg() {
+	return (
+		<img 
+			className="w-4 h-4 rounded-full"
+			src="https://webassets.linear.app/images/ornj730p/production/181a9d8a02fe6df34865175bc0097db476952862-512x512.jpg?w=72&amp;q=95&amp;auto=format&amp;dpr=2" 
+			alt="Avatar of Nan">
+		</img>
+	)
+}
+
+export function GetErinImg() {
+	return (
+		<img 
+			className="w-4 h-4 rounded-full"
+			src="https://webassets.linear.app/images/ornj730p/production/e71a8d13b2d26a3a53303810550d00b23cb87ce5-200x200.jpg?w=72&amp;q=95&amp;auto=format&amp;dpr=2"
+			alt="Avatar of Erin">
+		</img>
+	)
+}
--- src/components/ProductIntSection.jsx
+++ src/components/ProductIntSection.jsx
@@ -1,29 +1,21 @@
+import { GetErinImg, GetNanImg, GetYannImg } from "./Imgs";
+import { GetMobileIcon, GetSunIcon, GetTickIcon } from "./Svgs";
+
 function GetSuggestionComponent() {
 	return (
-		<div className="flex w-105 h-7 items-center gap-3">
-			<span className="text-sm text-[#8a8f98]">Suggestion</span>
+		<div className="flex w-105 h-7 items-center gap-3.5">
+			<span className="w-[76px] inline-block text-[0.8rem] text-[#8a8f98]">Suggestions</span>
 
 			<div className="flex items-center">
 				<button className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
-					<img 
-						className="w-4 h-4 rounded-full"
-						src="https://webassets.linear.app/images/ornj730p/production/e522be49c136f6b81a1c5cb3aa7b2075f78404c0-1305x1444.jpg?w=72&amp;q=95&amp;auto=format&amp;dpr=2" 
-						alt="Avatar of Nan">
-					</img>
+					<GetNanImg/>
 					<span className="text-sm text-[#f7f8f8]">nan</span>
 				</button>
 			</div>
 
 			<div className="flex items-center">
 				<button className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
-				<svg className="w-4 h-4" 
-					viewBox="0 0 16 16" 
-					fill="#5e6ad2" 
-					role="img" focusable="false" 
-					aria-hidden="true" 
-					xmlns="http://www.w3.org/2000/svg">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M10 2.5H10.5C11.0523 2.5 11.5 2.94772 11.5 3.5V12.5C11.5 13.0523 11.0523 13.5 10.5 13.5H5.5C4.94772 13.5 4.5 13.0523 4.5 12.5V3.5C4.5 2.94772 4.94772 2.5 5.5 2.5H6V3C6 3.55228 6.44772 4 7 4H9C9.55228 4 10 3.55228 10 3V2.5ZM3 3.5C3 2.11929 4.11929 1 5.5 1H10.5C11.8807 1 13 2.11929 13 3.5V12.5C13 13.8807 11.8807 15 10.5 15H5.5C4.11929 15 3 13.8807 3 12.5V3.5Z"></path>
-				</svg>
+					<GetMobileIcon/>
 					<span className="text-sm text-[#f7f8f8]">Mobile App Refactor</span>
 				</button>
 			</div>
@@ -41,8 +33,8 @@ function GetSuggestionComponent() {
 
 function GetPlainComponent({topic, text}) {
 	return (
-		<div className="flex w-105 h-7 items-center gap-3">
-			<span className="text-sm text-[#8a8f98]">{topic}</span>
+		<div className="flex w-105 h-7 items-center gap-3.5">
+			<span className="w-[76px] inline-block text-[0.8rem] text-[#8a8f98] tracking-tight">{topic}</span>
 
 			<div className="flex items-center">
 				<svg 
@@ -64,17 +56,66 @@ function GetPlainComponent({topic, text}) {
 			<div className="flex items-center">
 			</div>
 
-
 		</div>
 
 	);
 }
 
+function GetNanWindow() { 
+	return (
+		<div className="absolute py-3 px-4 w-85 h-67 top-1/2 left-1/4 -translate-x-1 -translate-y-0.5 bg-[#141516] border rounded-md border-[rgba(255,255,255,0.05)]">
+			<div className="flex flex-col">
+				<div className="flex pb-4 items-center gap-2 border-b border-[rgba(255,255,255,0.05)]">
+					<GetNanImg/>
+					<span className="text-sm text-[#f7f8f8]">nan</span>
+				</div>
+
+				<div className="py-2 pb-3 border-b border-[rgba(255,255,255,0.05)]">
+					<h3 className="text-[0.8rem] text-[#f7f8f8]">Why this assignee was suggested</h3>
+					<span className="text-[0.8rem] text-[#8a8f98]">
+						This person was the assignee on previous issues<br/>
+						related to performance problems in the mobile app<br/>
+						launch flow
+					</span>
+				</div>
+
+				<div className="py-2 pb-2">
+					<h3 className="text-[0.8rem] text-[#f7f8f8]">Alternatives</h3>
+
+					<div className="h-1.5"></div>
+
+					<div className="flex items-center gap-2">
+						<div className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
+							<GetYannImg/>
+							<span className="text-sm text-[#f7f8f8]">Yann</span>
+						</div>
+						<div className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
+							<GetErinImg/>
+							<span className="text-sm text-[#f7f8f8]">Erin</span>
+						</div>
+					</div>
+
+					<div className="h-2.5"></div>
+
+					<button className="absolute w-80.5 h-8 left-2 bg-[hsl(210,5%,13%)] rounded-md border border-[hsl(240,5%,20%)]">
+						<div className="flex items-center justify-center gap-2">
+							<GetTickIcon/>
+							<span className="text-[0.8rem] text-[#f7f8f8]">Accept suggestion</span>
+						</div>
+					</button>
+
+				</div>
+
+			</div>
+		</div>
+	);
+}
+
 export default function ProductIntSection() {
 	return (
-		<div className="relative w-110 h-40 p-5 bg-[#141516] rounded-md border border-[rgba(255,255,255,0.05)]">
+		<div className="relative w-110 h-40 px-4 py-4 bg-[#141516] rounded-md border border-[rgba(255,255,255,0.05)]">
 			<div className="flex items-center gap-2">
-				<SunIcon/>
+				<GetSunIcon/>
 				<span className="text-base text-[#f7f8f8]">Product Intelligence</span>
 			</div>
 
@@ -87,26 +128,54 @@ export default function ProductIntSection() {
 				<GetPlainComponent topic="Related to" text={"ENG - 1820 Mobile app takes long time to..."}/>
 			</div>
 
-		</div>
-	);
-}
+			{/* second window */}
 
-function SunIcon() {
-	return (
-		<svg 
-		className="w-4 h-4"
-		viewBox="0 0 16 16" 
-		fill="#9c9da1" 
-		role="img" 
-		focusable="false" 
-		aria-hidden="true" 
-		xmlns="http://www.w3.org/2000/svg">
-			<path fillRule="evenodd" clipRule="evenodd" d="M8.00098 11C8.41519 11 8.75098 11.3358 8.75098 11.75V14.25C8.75098 14.6642 8.41519 15 8.00098 15C7.58676 15 7.25098 14.6642 7.25098 14.25V11.75C7.25098 11.3358 7.58676 11 8.00098 11Z"></path>
-			<path fillRule="evenodd" clipRule="evenodd" d="M2.2207 2.21973C2.5136 1.92683 2.98836 1.92683 3.28125 2.21973L8.53125 7.46973C8.82403 7.76263 8.82411 8.23742 8.53125 8.53027L3.28125 13.7803C2.98839 14.0731 2.51359 14.073 2.2207 13.7803C1.92783 13.4874 1.92787 13.0126 2.2207 12.7197L5.33691 9.60352C5.65187 9.28855 5.4288 8.75006 4.9834 8.75H1.75098C1.3368 8.75 1.00104 8.41416 1.00098 8C1.00098 7.58579 1.33676 7.25 1.75098 7.25H4.9834C5.42877 7.24994 5.65178 6.71146 5.33691 6.39648L2.2207 3.28027C1.92783 2.9874 1.92787 2.51262 2.2207 2.21973Z"></path>
-			<path fillRule="evenodd" clipRule="evenodd" d="M9.9707 9.96973C10.2636 9.67683 10.7384 9.67683 11.0312 9.96973L13.7812 12.7197C14.0741 13.0126 14.0741 13.4874 13.7812 13.7803C13.4884 14.0731 13.0136 14.0731 12.7207 13.7803L9.9707 11.0303C9.67783 10.7374 9.67787 10.2626 9.9707 9.96973Z"></path>
-			<path fillRule="evenodd" clipRule="evenodd" d="M14.251 7.25C14.6652 7.25 15.001 7.58579 15.001 8C15.001 8.41421 14.6652 8.75 14.251 8.75H11.751C11.3368 8.75 11.001 8.41421 11.001 8C11.001 7.58579 11.3368 7.25 11.751 7.25H14.251Z"></path>
-			<path fillRule="evenodd" clipRule="evenodd" d="M12.7207 2.21973C13.0136 1.92683 13.4884 1.92683 13.7812 2.21973C14.0741 2.51262 14.0741 2.98738 13.7812 3.28027L11.0312 6.03027C10.7384 6.32311 10.2636 6.32315 9.9707 6.03027C9.67783 5.7374 9.67787 5.26262 9.9707 4.96973L12.7207 2.21973Z"></path>
-			<path fillRule="evenodd" clipRule="evenodd" d="M8.00098 1C8.41519 1 8.75098 1.33579 8.75098 1.75V4.25C8.75098 4.66421 8.41519 5 8.00098 5C7.58676 5 7.25098 4.66421 7.25098 4.25V1.75C7.25098 1.33579 7.58676 1 8.00098 1Z"></path>
-		</svg>
+			<div className="absolute py-3 px-4 w-85 h-67 top-1/2 left-1/4 -translate-x-1 -translate-y-0.5 bg-[#141516] border rounded-md border-[rgba(255,255,255,0.05)]">
+				<div className="flex flex-col">
+					<div className="flex pb-4 items-center gap-2 border-b border-[rgba(255,255,255,0.05)]">
+						<GetNanImg/>
+						<span className="text-sm text-[#f7f8f8]">nan</span>
+					</div>
+
+					<div className="py-2 pb-3 border-b border-[rgba(255,255,255,0.05)]">
+						<h3 className="text-[0.8rem] text-[#f7f8f8]">Why this assignee was suggested</h3>
+						<span className="text-[0.8rem] text-[#8a8f98]">
+							This person was the assignee on previous issues<br/>
+							related to performance problems in the mobile app<br/>
+							launch flow
+						</span>
+					</div>
+
+					<div className="py-2 pb-2">
+						<h3 className="text-[0.8rem] text-[#f7f8f8]">Alternatives</h3>
+
+						<div className="h-1.5"></div>
+
+						<div className="flex items-center gap-2">
+							<div className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
+								<GetYannImg/>
+								<span className="text-sm text-[#f7f8f8]">Yann</span>
+							</div>
+							<div className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
+								<GetErinImg/>
+								<span className="text-sm text-[#f7f8f8]">Erin</span>
+							</div>
+						</div>
+
+						<div className="h-2.5"></div>
+
+						<button className="absolute w-80.5 h-8 left-2 bg-[hsl(210,5%,13%)] rounded-md border border-[hsl(240,5%,20%)]">
+							<div className="flex items-center justify-center gap-2">
+								<GetTickIcon/>
+								<span className="text-[0.8rem] text-[#f7f8f8]">Accept suggestion</span>
+							</div>
+						</button>
+
+					</div>
+
+				</div>
+		</div>
+
+		</div>
 	);
 }
