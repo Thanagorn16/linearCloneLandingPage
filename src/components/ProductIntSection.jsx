b9f6d96993fb02b5746cebd11bf99a2173be6277
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2025-09-11 20:34:28 +0700

    added: mobile window

--- src/components/ProductIntSection.jsx
+++ src/components/ProductIntSection.jsx
@@ -1,5 +1,5 @@
 import { GetErinImg, GetNanImg, GetYannImg } from "./Imgs";
-import { GetMobileIcon, GetSunIcon, GetTickIcon } from "./Svgs";
+import { GetHexagonIcon, GetMobileIcon, GetMobileSignalIcon, GetSunIcon, GetTickIcon } from "./Svgs";
 
 function GetSuggestionComponent() {
 	return (
@@ -131,36 +131,39 @@ export default function ProductIntSection() {
 			{/* second window */}
 
 			<div className="absolute py-3 px-4 w-85 h-67 top-1/2 left-1/4 -translate-x-1 -translate-y-0.5 bg-[#141516] border rounded-md border-[rgba(255,255,255,0.05)]">
+				{/* col for the whole window */}
 				<div className="flex flex-col">
-					<div className="flex pb-4 items-center gap-2 border-b border-[rgba(255,255,255,0.05)]">
-						<GetNanImg/>
-						<span className="text-sm text-[#f7f8f8]">nan</span>
+					{/* col for the first part */}
+					<div className="flex flex-col pb-3 gap-1 border-b border-[rgba(255,255,255,0.05)]">
+						<div className="flex items-center gap-2">
+							<GetMobileIcon/>
+							<span className="text-sm text-[#f7f8f8]">Mobile App Refactor</span>
+						</div>
+						<span className="text-sm text-[#8a8f98]">Optimize mobile app performance</span>
+
+						<div className="flex gap-4">
+							<div className="flex items-center gap-1">
+								<GetHexagonIcon/>
+								<span className="text-sm text-[#f7f8f8]">In Progress</span>
+							</div>
+							<div className="flex items-center gap-1">
+								<GetMobileSignalIcon/>
+								<span className="text-sm text-[#f7f8f8]">Medium</span>
+							</div>
+						</div>
 					</div>
 
-					<div className="py-2 pb-3 border-b border-[rgba(255,255,255,0.05)]">
-						<h3 className="text-[0.8rem] text-[#f7f8f8]">Why this assignee was suggested</h3>
-						<span className="text-[0.8rem] text-[#8a8f98]">
-							This person was the assignee on previous issues<br/>
-							related to performance problems in the mobile app<br/>
-							launch flow
-						</span>
+					<div className="py-3 pb-3 border-b border-[rgba(255,255,255,0.05)]">
+						<h3 className="text-[0.8rem] text-[#f7f8f8]">Why this project was suggested</h3>
+						<div className="h-1"></div>
+						<p className=" text-[0.8rem] text-[#8a8f98] tracking-[-0.1px]">
+							This current issue seems to be a mobile app related<br/>
+							bug that would fit well within the scope of this project.
+						</p>
 					</div>
 
 					<div className="py-2 pb-2">
-						<h3 className="text-[0.8rem] text-[#f7f8f8]">Alternatives</h3>
 
-						<div className="h-1.5"></div>
-
-						<div className="flex items-center gap-2">
-							<div className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
-								<GetYannImg/>
-								<span className="text-sm text-[#f7f8f8]">Yann</span>
-							</div>
-							<div className="flex px-[6px] py-[1px] items-center gap-1.5 border border-dashed border-[#3e3e44]">
-								<GetErinImg/>
-								<span className="text-sm text-[#f7f8f8]">Erin</span>
-							</div>
-						</div>
 
 						<div className="h-2.5"></div>
 
